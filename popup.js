1d253117029b00657f83cba5aa8ccd3354c606ba
Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>
2013-03-02 16:21:23 +0530

    Adds GPL v3 License to the software

--- popup.js
+++ popup.js
@@ -1,3 +1,25 @@
+/*
+*  Javascript for Popup Page.
+*
+* This file is part of IITG Moodle Notifier
+*
+* IITG Moodle Notifier is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* IITG Moodle Notifier is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with IITG Moodle Notifier. If not, see <http://www.gnu.org/licenses/>.
+*
+* Copyright (C) 2013, Sai Manoj Kumar Yadlapati <ysaimanojkumar@gmail.com>, India.
+*
+*/
+
 
 window.onload = function(){
 	
